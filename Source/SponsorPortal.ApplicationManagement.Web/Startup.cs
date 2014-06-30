ddc76609cc935dccc25642fc8980a5749d398de2
Eirik Årdal <eaardal@outlook.com>
2014-06-30 20:26:50 +0200

    Moved all projects into \Source folder for more flexible project structure

--- Source/SponsorPortal.ApplicationManagement.Web/Startup.cs
+++ Source/SponsorPortal.ApplicationManagement.Web/Startup.cs
@@ -1,35 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Autofac;
+using Microsoft.Owin;
+using Microsoft.Owin.Diagnostics;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.WebApi;
 using Owin;
+using SponsorPortal.ApplicationManagement.Core.CommandModel;
+using SponsorPortal.ApplicationManagement.Core.Commands;
+using SponsorPortal.ApplicationManagement.Core.QueryModel;
+using SponsorPortal.ApplicationManagement.Web;
+using SponsorPortal.EventStore;
+using SponsorPortal.Infrastructure;
 
+[assembly: OwinStartup(typeof(Startup))]
 namespace SponsorPortal.ApplicationManagement.Web
 {
     public class Startup
     {
         public void Configuration(IAppBuilder builder)
         {
-            var ioc = new ContainerBuilder();
-            ioc.RegisterType<>()
+            var container = ConfigureIoC();
+            InitializeEventStore(container);
+            InitializeProjections(container);
+            ConfigureErrorHandling(builder);
+            ConfigureWebApi(builder, container);
+            
+        }
 
+        private void ConfigureErrorHandling(IAppBuilder builder)
+        {
+            builder.UseErrorPage(new ErrorPageOptions
+            {
+                ShowExceptionDetails = true,
+                ShowEnvironment = true,
+                ShowCookies = false,
+                ShowSourceCode = true,
+            });
+        }
+
+        private IUnityContainer ConfigureIoC()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<ICommandHandler<CreateNewApplicationFormCommand>, ApplicationFormService>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IApplicationFormProjection, ApplicationFormProjection>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IApplicationFormRespository, ApplicationFormRepository>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IEventPersistance, EventStoreEventPersistance>(new ContainerControlledLifetimeManager());
+            container.RegisterType<ICommandDispatcher, CommandDispatcher>();
+            container.RegisterType<ApplicationFormController>();
+
+            /*
+            container.RegisterTypes(
+                AllClasses.FromAssemblies(Assembly.GetExecutingAssembly())
+                    .Where(t => typeof (Projection).IsAssignableFrom(t)));
+            */
+
+            IoC.RegisterContainer(container);
+            return container;
+        }
+
+        private void InitializeEventStore(IUnityContainer container)
+        {
+            var eventstore = container.Resolve<IEventPersistance>();
+            eventstore.Initialize();
+        }
+
+        private async void InitializeProjections(IUnityContainer container)
+        {
+            var projection = container.Resolve<ApplicationFormProjection>();
+            await projection.GetAllExistingEventsOfInterest();
+        }
+
+        private void ConfigureWebApi(IAppBuilder builder, IUnityContainer container)
+        {
             var config = new HttpConfiguration();
             config.MapHttpAttributeRoutes();
-            config.DependencyResolver = 
+            config.DependencyResolver = new UnityDependencyResolver(container);
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
             builder.UseWebApi(config);
-        }
-    }
-
-    public class TestController : ApiController
-    {
-        [HttpGet]
-        [Route("test")]
-        public string Get()
-        {
-            return "hello from webapi owin...";
         }
     }
 }
--- Source/__SponsorPortal.ApplicationManagement.Web/Properties/AssemblyInfo.cs
+++ Source/__SponsorPortal.ApplicationManagement.Web/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SponsorPortal.Management")]
+[assembly: AssemblyTitle("SponsorPortal.ApplicationManagement.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SponsorPortal.Management")]
+[assembly: AssemblyProduct("SponsorPortal.ApplicationManagement.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("267af71f-b4c8-44c4-b75f-1e4bb95480fa")]
+[assembly: Guid("6c3fd2e3-c835-4077-b7dd-e42987bc2366")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
